965d8a160e139f84059ecf4cd5903d81a34cce6e
Marcel de Groot <mg.degroot@avans.nl>
2022-11-11 15:27:57 +0100

    dummy component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DummyComponent } from './dummy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "dummy", pathMatch: 'full', component: DummyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DummyComponent } from './dummy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DummyComponent
   ],
   imports: [
     BrowserModule,
